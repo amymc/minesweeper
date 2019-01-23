eb1178630ebd194fbb0f0390004a0f724b8ce55c
Darío Javier Cravero <dario@uxtemple.com>
2019-01-23 12:14:45 +0000

    feat: extract pressed logic into a custom hook

--- src/Header/Status.view.logic.js
+++ src/Header/Status.view.logic.js
@@ -2,10 +2,7 @@ import Status from './Status.view.js'
 import React, { useState } from 'react'
 
 let StatusLogic = props => {
-  let [isPressed, setIsPressed] = useState(false)
-
-  let onMouseDown = () => setIsPressed(true)
-  let onMouseUp = () => setIsPressed(false)
+  let { isPressed, onMouseDown, onMouseUp } = usePressed()
 
   return (
     <Status
@@ -18,3 +15,16 @@ let StatusLogic = props => {
 }
 
 export default StatusLogic
+
+let usePressed = () => {
+  let [isPressed, setIsPressed] = useState(false)
+
+  let onMouseDown = () => setIsPressed(true)
+  let onMouseUp = () => setIsPressed(false)
+
+  return {
+    isPressed,
+    onMouseDown,
+    onMouseUp,
+  }
+}
