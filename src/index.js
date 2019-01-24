643735931460a04f2713a1a34a4c054d5dfbcf93
Darío Javier Cravero <dario@uxtemple.com>
2019-01-24 15:23:48 +0000

    feat: use React.lazy to do code splitting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./Main/App.view.logic.js"
-import * as serviceWorker from "./serviceWorker"
-import { AppProvider } from "./App.context.js"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './Main/App.view.logic.js'
+import * as serviceWorker from './serviceWorker'
+import { AppProvider } from './App.context.js'
 
 ReactDOM.render(
-  <AppProvider>
-    <App />
-  </AppProvider>,
-  document.getElementById("root")
+  <React.Suspense fallback={<div>LOADING</div>}>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.Suspense>,
+  document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
